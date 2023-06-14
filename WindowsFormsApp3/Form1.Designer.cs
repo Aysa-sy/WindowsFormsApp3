6d0a1c1474247fd29a520e52ef0e216d9a5c4f93
aysa-sy <aisa.soleimani83@gmail.com>
2023-06-14 23:40:17 +0330

    Add project files.

---
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿
+namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_Name = new System.Windows.Forms.Label();
+            this.txt_Name = new System.Windows.Forms.TextBox();
+            this.txt_Family = new System.Windows.Forms.TextBox();
+            this.txt_Age = new System.Windows.Forms.TextBox();
+            this.txt_Address = new System.Windows.Forms.TextBox();
+            this.lbl_Fmily = new System.Windows.Forms.Label();
+            this.lbl_Age = new System.Windows.Forms.Label();
+            this.lbl_Address = new System.Windows.Forms.Label();
+            this.bt_Ok = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl_Name
+            // 
+            this.lbl_Name.AutoSize = true;
+            this.lbl_Name.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.lbl_Name.Location = new System.Drawing.Point(55, 42);
+            this.lbl_Name.Name = "lbl_Name";
+            this.lbl_Name.Size = new System.Drawing.Size(85, 29);
+            this.lbl_Name.TabIndex = 0;
+            this.lbl_Name.Text = "name :";
+            // 
+            // txt_Name
+            // 
+            this.txt_Name.Location = new System.Drawing.Point(157, 49);
+            this.txt_Name.Name = "txt_Name";
+            this.txt_Name.Size = new System.Drawing.Size(197, 22);
+            this.txt_Name.TabIndex = 1;
+            this.txt_Name.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txt_Family
+            // 
+            this.txt_Family.Location = new System.Drawing.Point(157, 96);
+            this.txt_Family.Name = "txt_Family";
+            this.txt_Family.Size = new System.Drawing.Size(197, 22);
+            this.txt_Family.TabIndex = 2;
+            this.txt_Family.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // txt_Age
+            // 
+            this.txt_Age.Location = new System.Drawing.Point(157, 144);
+            this.txt_Age.Name = "txt_Age";
+            this.txt_Age.Size = new System.Drawing.Size(197, 22);
+            this.txt_Age.TabIndex = 3;
+            // 
+            // txt_Address
+            // 
+            this.txt_Address.Location = new System.Drawing.Point(157, 192);
+            this.txt_Address.Name = "txt_Address";
+            this.txt_Address.Size = new System.Drawing.Size(197, 22);
+            this.txt_Address.TabIndex = 4;
+            // 
+            // lbl_Fmily
+            // 
+            this.lbl_Fmily.AutoSize = true;
+            this.lbl_Fmily.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.lbl_Fmily.Location = new System.Drawing.Point(53, 96);
+            this.lbl_Fmily.Name = "lbl_Fmily";
+            this.lbl_Fmily.Size = new System.Drawing.Size(87, 29);
+            this.lbl_Fmily.TabIndex = 5;
+            this.lbl_Fmily.Text = "family :";
+            // 
+            // lbl_Age
+            // 
+            this.lbl_Age.AutoSize = true;
+            this.lbl_Age.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.lbl_Age.Location = new System.Drawing.Point(74, 144);
+            this.lbl_Age.Name = "lbl_Age";
+            this.lbl_Age.Size = new System.Drawing.Size(66, 29);
+            this.lbl_Age.TabIndex = 6;
+            this.lbl_Age.Text = "age :";
+            // 
+            // lbl_Address
+            // 
+            this.lbl_Address.AutoSize = true;
+            this.lbl_Address.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.lbl_Address.Location = new System.Drawing.Point(28, 192);
+            this.lbl_Address.Name = "lbl_Address";
+            this.lbl_Address.Size = new System.Drawing.Size(112, 29);
+            this.lbl_Address.TabIndex = 7;
+            this.lbl_Address.Text = "address :";
+            // 
+            // bt_Ok
+            // 
+            this.bt_Ok.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.bt_Ok.Location = new System.Drawing.Point(122, 258);
+            this.bt_Ok.Name = "bt_Ok";
+            this.bt_Ok.Size = new System.Drawing.Size(75, 37);
+            this.bt_Ok.TabIndex = 8;
+            this.bt_Ok.Text = "ok";
+            this.bt_Ok.UseVisualStyleBackColor = true;
+            this.bt_Ok.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.button1.Location = new System.Drawing.Point(222, 258);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 37);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Clear";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.button2.Location = new System.Drawing.Point(327, 258);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 37);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Exit";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.bt_Ok);
+            this.Controls.Add(this.lbl_Address);
+            this.Controls.Add(this.lbl_Age);
+            this.Controls.Add(this.lbl_Fmily);
+            this.Controls.Add(this.txt_Address);
+            this.Controls.Add(this.txt_Age);
+            this.Controls.Add(this.txt_Family);
+            this.Controls.Add(this.txt_Name);
+            this.Controls.Add(this.lbl_Name);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_Name;
+        private System.Windows.Forms.TextBox txt_Name;
+        private System.Windows.Forms.TextBox txt_Family;
+        private System.Windows.Forms.TextBox txt_Age;
+        private System.Windows.Forms.TextBox txt_Address;
+        private System.Windows.Forms.Label lbl_Fmily;
+        private System.Windows.Forms.Label lbl_Age;
+        private System.Windows.Forms.Label lbl_Address;
+        private System.Windows.Forms.Button bt_Ok;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (txt_Name.Text != "")
+            {
+                if (txt_Family.Text != "")
+                {
+                    if (txt_Age.Text != "")
+                    {
+                        if (txt_Address.Text != "")
+                        {
+                            string txt = txt_Name.Text + " " + txt_Family.Text + " " + txt_Age.Text + " " + txt_Address.Text;
+                            MessageBox.Show(txt);
+                        }
+                        else
+                        {
+                            MessageBox.Show("نام وارد نشده");
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("فامیلی وارد نشده");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("سن وارد نشده");
+                }
+            }
+            else
+            {
+                MessageBox.Show("آدرس وارد نشده");
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
